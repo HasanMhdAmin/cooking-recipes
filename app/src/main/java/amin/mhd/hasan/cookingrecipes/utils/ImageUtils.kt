074f998e06650dfa4aec7c7dff8c1292f2dca519
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2020-10-31 19:12:58 +0100

    Reduce image size

--- app/src/main/java/amin/mhd/hasan/cookingrecipes/utils/ImageUtils.kt
+++ app/src/main/java/amin/mhd/hasan/cookingrecipes/utils/ImageUtils.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
 
+
 /**
  * Created by Hasan Mhd Amin on 30.10.20
  */
@@ -24,27 +25,25 @@ class ImageUtils {
             imageName: String
         ): File {
             val cw = ContextWrapper(context)
-            val directory: File = cw.getDir("recipe_images", Context.MODE_PRIVATE)
+            val directory: File = cw.getDir(folderName, Context.MODE_PRIVATE)
             if (!directory.exists()) {
                 directory.mkdir()
             }
-
-
-            val mypath = File(directory, imageName)
+            val myPath = File(directory, imageName)
 
             var fos: FileOutputStream? = null
             try {
-                fos = FileOutputStream(mypath)
-                val bitmap: Bitmap
+                fos = FileOutputStream(myPath)
+                var bitmap: Bitmap
                 if (Build.VERSION.SDK_INT < 28) {
                     bitmap = MediaStore.Images.Media.getBitmap(
-                        context?.contentResolver,
+                        context.contentResolver,
                         selectedImage
                     )
                 } else {
                     val source =
-                        selectedImage?.let {
-                            context?.contentResolver?.let { ctx ->
+                        selectedImage.let {
+                            context.contentResolver?.let { ctx ->
                                 ImageDecoder.createSource(
                                     ctx,
                                     it
@@ -54,13 +53,37 @@ class ImageUtils {
                     bitmap = ImageDecoder.decodeBitmap(source!!)
                 }
 
-                bitmap.compress(Bitmap.CompressFormat.PNG, 5, fos)
+                bitmap = resize(bitmap, 512, 512)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fos)
                 fos.close()
             } catch (e: Exception) {
                 Log.e("SAVE_IMAGE", e.message, e)
             }
-            return mypath
+            return myPath
         }
+
+
+        private fun resize(image: Bitmap, maxWidth: Int, maxHeight: Int): Bitmap {
+            var image = image
+            return if (maxHeight > 0 && maxWidth > 0) {
+                val width = image.width
+                val height = image.height
+                val ratioBitmap = width.toFloat() / height.toFloat()
+                val ratioMax = maxWidth.toFloat() / maxHeight.toFloat()
+                var finalWidth = maxWidth
+                var finalHeight = maxHeight
+                if (ratioMax > 1) {
+                    finalWidth = (maxHeight.toFloat() * ratioBitmap).toInt()
+                } else {
+                    finalHeight = (maxWidth.toFloat() / ratioBitmap).toInt()
+                }
+                image = Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true)
+                image
+            } else {
+                image
+            }
+        }
+
 
     }
 }
